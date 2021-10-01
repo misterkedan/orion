8c9a6faafdb8a55ba80176bf05b76ba121bfbd07
Pierre Keda <pierrekeda83@gmail.com>
2021-10-01 19:38:56 +0200

    Fix page reload on first random

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+import winlo from 'winlo';
+
 import { App } from './scene/alien/App';
 
-import { controls } from './controls';
+if ( ! winlo.search ) {
+
+	winlo.hash = '#/';
+	winlo.search = '';
+
+}
 
 App.init();
 
